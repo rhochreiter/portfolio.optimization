6fb762d81d33d8ecc01ea4b8f430cbd72e05f904
Ronald Hochreiter <ron@hochreiter.net>
2018-08-11 09:03:21 +0200

    updated tutorial 101 and added function to open tutorials

---
+++ R/po.tutorial.R
@@ -0,0 +1,27 @@
+#' @title Open a specific portfolio.optimization package tutorial 
+#'
+#' @description
+#' \code{po.tutorial} opens a specific portfolio.optimization package tutorial.
+#' If no tutorial is given or the tutorial is missspelled, a list of available
+#' tutorials is printed.
+#'
+#' @param tutorial name of the tutorial to open 
+#' 
+#' @return NULL
+#' 
+#' @author Ronald Hochreiter, \email{ronald@@algorithmic.finance}
+#'
+#' @export
+po.tutorial <- function(tutorial="") {
+  tutorials <- c("101")
+  tutorial_files <- c("1-po101")
+  
+  if(!tutorial %in% tutorials) {
+    print(paste0("Available tutorials: ", paste(tutorials, collapse=", ")))
+  }
+  else {
+    tutorial_pos <- which(tutorials == tutorial)
+    file.edit(paste0(path.package("portfolio.optimization"), "/", tutorial_files[tutorial_pos], ".R"))
+  }
+  return(NULL)
+}
--- inst/1-po101.R
+++ inst/1-po101.R
@@ -1,25 +1,37 @@
-# Libraries
-library(portfolio.optimization)
-library(xts)
+### 1. Data
 
-# 1. Load demo data
+# Use daily-based (crude) weekly return data from the S&P 100 in 2017
 data(sp100w17)
 
+# Only use 30 assets with the highest average trading volume during 2017
 sort.by.volume <- sort(sp100w17av, index.return=TRUE, decreasing = TRUE)
 top30.volume <- sort(sort.by.volume$ix[1:30])
 
+# Create the scenario set with those
 scenario.set <- sp100w17[, top30.volume]
 
-# 2. Conduct an optimization with default parameters (Long-only Markowitz)
-model <- optimal.portfolio(data)
+### 2. Optimization with default parameters (Long-only Markowitz)
+
+# One-Liner
+model <- optimal.portfolio(scenario.set)
+
+# Plot the result
 barplot(model)
 
-# 3. Similar optimization, but explicitely create a model
-model <- portfolio.model(data)
+### 3. Repeat optimization but explicitely create a model
+
+model <- portfolio.model(scenario.set)
 markowitz <- optimal.portfolio(model)
 
-# 4. Change one parameter of the model (objective function and repeat optimization)
+### 4. Compute a long-only MAD portfolio
+
+# We do so by changing one parameter of the model, i.e. the specification
+# of the objective function and repeat the optimization
+
 mad <- optimal.portfolio(objective(model, "mad"))
 
-# 5. Plot differences between Markowitz and MAD
-barplot(matrix(c(x(markowitz), x(mad)), nrow=2, byrow=TRUE), beside=TRUE, las=3, names.arg=names(data), col=topo.colors(2), legend=c("Markowitz", "MAD"))
+### 5. Plot portfolio differences between Markowitz and MAD
+
+barplot(matrix(c(x(markowitz), x(mad)), nrow=2, byrow=TRUE), 
+        beside=TRUE, las=3, names.arg=names(data), 
+        col=topo.colors(2), legend=c("Markowitz", "MAD"))
