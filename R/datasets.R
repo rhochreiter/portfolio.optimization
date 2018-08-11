3127df9d4ef6ce7e8321595e819a58bf5f0ec8f5
Ronald Hochreiter <ron@hochreiter.net>
2018-08-11 09:13:16 +0200

    added description of sp100w17

---
+++ R/datasets.R
@@ -0,0 +1,26 @@
+#' S&P 100 weekly stock returns 2017
+#'
+#' A dataset sp100w17 containing the (crude) weekly returns of (almost) all 
+#' S&P 100 stocks of 2017, daily basis (101 stocks, 251 returns). 
+#' 
+#' Furthermore contains a vector sp100w17av with the average trading volume
+#' of all stocks in 2017 - to be used for a subselection.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sp100w17
+#' @usage data(sp100w17)
+#' @format One xts time series object with 251 rows and 101 columns.
+NULL
+
+#' S&P 100 average trading volume over the whole year 2017
+#' 
+#' A vector sp100w17av with the average trading volume
+#' of all stocks in 2017 - to be used e.g. for a subselection.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sp100w17av
+#' @usage data(sp100w17)
+#' @format One named numeric vector of length 101.
+NULL
