3a4363282025d25784243d44bdcd0f939c1e162b
Ronald Hochreiter <ron@hochreiter.net>
2018-08-11 12:07:10 +0200

    further polishing tutorial 101

--- inst/1-po101.R
+++ inst/1-po101.R
@@ -9,6 +9,7 @@ top30.volume <- sort(sort.by.volume$ix[1:30])
 
 # Create the scenario set with those
 scenario.set <- sp100w17[, top30.volume]
+rm(sp100w17, sp100w17av, sort.by.volume, top30.volume)
 
 ### 2. Optimization with default parameters (Long-only Markowitz)
 
