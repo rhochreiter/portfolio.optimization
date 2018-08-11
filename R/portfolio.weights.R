ce27537098047e3019a1368bb445d778b9cf0b7f
Ronald Hochreiter <ron@hochreiter.net>
2018-08-11 10:37:38 +0200

    added portfolio.weights from scenportopt

---
+++ R/portfolio.weights.R
@@ -0,0 +1,16 @@
+#' @title Return the portfolio weights of a portfolio.model
+#' 
+#' @description
+#' \code{portfolio.weights} return the portfolio weights of a portfolio.model
+#'
+#' @param model the portfolio.model to return the portfolio weights from
+#' 
+#' @return a vector of portfolio weights or NULL if no weights are available yet.
+#' 
+#' @author Ronald Hochreiter, \email{ronald@@algorithmic.finance}
+#'
+#' @export
+portfolio.weights <- x <- function(model) {
+  if (is.null(model$portfolio)) { return(NULL) } 
+  else { return(model$portfolio$x) }
+}
