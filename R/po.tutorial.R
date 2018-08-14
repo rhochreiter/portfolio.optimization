b0947299d9d6990086d4d9c0bbbc87c20636b45e
Ronald Hochreiter <ron@hochreiter.net>
2018-08-14 05:34:16 +0200

    removed file.edit from po.tutorial

--- R/po.tutorial.R
+++ R/po.tutorial.R
@@ -1,30 +1,26 @@
 #' @title Open a specific portfolio.optimization package tutorial 
 #'
 #' @description
-#' \code{po.tutorial} opens a specific portfolio.optimization package tutorial.
-#' If no tutorial is given or the tutorial is missspelled, a list of available
-#' tutorials is printed.
+#' \code{po.tutorial} returns the filename of a specific portfolio.optimization 
+#' package tutorial. If no tutorial is given or the tutorial is missspelled, a 
+#' list of available tutorials is printed.
 #'
 #' @param tutorial name of the tutorial to open 
 #' 
-#' @return Nothing.
+#' @return Nothing if no tutorial specified, otherwise the path to the tutorial.
 #' 
 #' @author Ronald Hochreiter, \email{ronald@@algorithmic.finance}
 #'
-# Problem with importFrom - RStudio uses wrong editor if enabled
-# @importFrom utils file.edit
-#'
 #' @export
 po.tutorial <- function(tutorial="") {
-  tutorials <- c("101")
-  tutorial_files <- c("1-po101")
+  tutorials <- c("101", "compare")
+  tutorial_files <- c("1-po101", "2-compare")
   
   if(!tutorial %in% tutorials) {
     print(paste0("Available tutorials: ", paste(tutorials, collapse=", ")))
   }
   else {
     tutorial_pos <- which(tutorials == tutorial)
-    file.edit(paste0(path.package("portfolio.optimization"), "/", tutorial_files[tutorial_pos], ".R"),
-              editor="internal")
+    return(paste0(path.package("portfolio.optimization"), "/", tutorial_files[tutorial_pos], ".R"))
   }
 }
