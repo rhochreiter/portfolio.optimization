a263f4f64b9cbcbc14f387b7d6994a7af11756a9
Ronald Hochreiter <ron@hochreiter.net>
2018-08-18 18:35:11 +0200

    added the 30 asset scenario matrix

--- R/datasets.R
+++ R/datasets.R
@@ -24,3 +24,16 @@ NULL
 #' @usage data(sp100w17)
 #' @format One named numeric vector of length 101.
 NULL
+
+#' S&P 100 weekly stock returns 2017 of 30 stocks with the highest average
+#' trading volume over the whole year
+#'
+#' A sceario sp100w17 containing the (crude) weekly returns of (almost) all 
+#' S&P 100 stocks of 2017, daily basis (101 stocks, 251 returns). 
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sp100w17av30s
+#' @usage data(sp100w17av30s)
+#' @format A named matrix scenario.set with 251 rows and 30 columns.
+NULL
